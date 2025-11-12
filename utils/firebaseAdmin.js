4a9f28ccc9591824cadce0535b0ca707e28e02f5
riyad-96 <riyadofficial.ide@gmail.com>
2025-11-12 11:34:40 +0600

    service key encoded

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -1,7 +1,9 @@
 // External modules
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../serviceAccountKey.json');
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, 'base64').toString('utf-8');
+
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
